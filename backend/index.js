7ef2af5921b708cbaed077ba7aa1b66caba80c26
harshita1611 <harshitabhamhani5@gmail.com>
2024-01-27 12:52:21 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const supabase = require('./supabase');
-
-
+const jwt = require('jsonwebtoken');
+const userRouter =require('./routes/user');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+supabase.from('UserLogin').select('*').then((response)=>{
+    console.log(response);
+    console.log("Success")
+}).catch((error)=>{
+    console.log(error);
+    console.log("Error")
+})
+
+app.use('/user', userRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-    });
+});
 
 
 app.listen(3000, () => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,3 +9,27 @@ const app = Router();
 app.use(express.json());
 
 
+app.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const { data, error } = await supabase.from('UserLogin').insert([{ username, email, password }])
+        if (error) {
+            console.error('Error saving user data to Supabase:', error.message);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        console.log('User data saved to Supabase:', data);
+        res.status(200).json({ msg: 'User registration successful' });
+    } catch (error) {
+        console.error('Exception during user registration:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+module.exports = app;
