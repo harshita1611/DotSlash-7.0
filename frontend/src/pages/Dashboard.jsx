61dbb1da4c7f4f849030cfefee3fee3ccc33278d
harshita1611 <harshitabhamhani5@gmail.com>
2024-01-28 02:14:49 +0530

    top gainer loser api

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,40 +3,45 @@ import Graph from '../components/Graph'
 import Navbar from "../components/navbar";
 
 function Dashboard() {
-   
     const [topGainers, setTopGainers] = useState([]);
     const [topLosers, setTopLosers] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:3000/user/dashboard")
-            .then(response => response.json())
-            .then(data => {
-                if (data && data.topGainersList && data.topLosersList) {
-                    setTopGainers(data.topGainersList);
-                    setTopLosers(data.topLosersList);
-                }
-            })
-            .catch(error => console.error("Error fetching dashboard data:", error));
+        fetchTopGainersAndLosers();
     }, []);
+
+    const fetchTopGainersAndLosers = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/user/dashboard");
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const data = await response.json();
+            setTopGainers(data.topGainers || []);
+            setTopLosers(data.topLosers || []);
+        } catch (error) {
+            console.error("Error fetching dashboard data:", error);
+        }
+    };
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="flex gap-10 p-5">
                 <div style={{ flex: '60%' }}>
-                    <Graph/>
+                    <Graph />
                 </div>
                 <div style={{ flex: '1' }}>
                     <h2>Top Gainers</h2>
                     <ul>
-                        {topGainers && topGainers.map((gainer, index) => (
+                        {topGainers.map((gainer, index) => (
                             <li key={index}>{gainer.ticker}: [{gainer.price}, {gainer.change_percentage}] </li>
                         ))}
                     </ul>
                     <h2>Top Losers</h2>
                     <ul>
-                        {topLosers && topLosers.map((loser, index) => (
-                            <li key={index}>{loser.ticker}: {loser.price}</li>
+                        {topLosers.map((loser, index) => (
+                            <li key={index}>{loser.ticker}: [{loser.price},{loser.change_percentage}]</li>
                         ))}
                     </ul>
                 </div>
