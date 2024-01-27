3066ecb71b5bf1ff2380283a2bcb845378d8c18c
harshita1611 <harshitabhamhani5@gmail.com>
2024-01-28 03:43:44 +0530

    dahsboard ho gyaaaaaa graph ke saath ho gya guysss

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 import Papa from 'papaparse';
 
-
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Graph extends Component {
@@ -17,11 +16,20 @@ class Graph extends Component {
     this.fetchCSVData();
   }
   
+  componentDidUpdate(prevProps) {
+    if (this.props.selectedTicker !== prevProps.selectedTicker) {
+      this.fetchCSVData();
+    }
+  }
+  
   fetchCSVData() {
-    fetch('../public/datasets/daily/AAPL.csv')
+    const { selectedTicker } = this.props;
+    if (!selectedTicker) return;
+    console.log(selectedTicker)
+    fetch(`../public/datasets/daily/${selectedTicker}.csv`)
       .then((response) => response.text())
       .then((csvData) => {
-        console.log('CSV Data:', csvData); 
+        console.log(csvData)
         this.parseCSVData(csvData);
       })
       .catch((error) => {
@@ -32,15 +40,13 @@ class Graph extends Component {
   parseCSVData(csvData) {
     Papa.parse(csvData, {
       header: true,
-      dynamicTyping: (field) => (field === 'Date' ? false : true), 
+      dynamicTyping: (field) => (field === 'Date' ? false : true),
       complete: (result) => {
         const dataPoints = result.data.map((row) => ({
           x: new Date(row.Date),
           y: [parseFloat(row['1. open']), parseFloat(row['2. high']), parseFloat(row['3. low']), parseFloat(row['4. close'])],
         }));
-  
-        console.log('Data Points:', dataPoints);
-  
+
         this.setState({ dataPoints });
       },
     });
@@ -54,12 +60,10 @@ class Graph extends Component {
       animationEnabled: true,
       exportEnabled: true,
       title: {
-        text: 'Intel Corporation Stock Price - 2017',
+        text: 'Stock Price',
       },
       axisX: {
         valueFormatString: 'YYYY-MM-DD',
-
-
       },
       axisY: {
         includeZero: false,
@@ -68,29 +72,29 @@ class Graph extends Component {
       },
       data: [
         {
-          type: 'area', // Change type to 'area' 
+          type: 'area',
           showInLegend: false,
           name: 'Stock Price',
           dataPoints: dataPoints.map((point) => ({
             x: point.x,
-            y: point.y[3], // Close price for the area chart
+            y: point.y[3],
           })),
         },
         {
-          type: 'line', // Add a line series for the closing prices
+          type: 'line',
           showInLegend: true,
           name: 'Close Price',
           dataPoints: dataPoints.map((point) => ({
             x: point.x,
-            y: point.y[3], // Closing price for the line chart
+            y: point.y[3],
           })),
         },
       ],
     };
 
     return (
-      <div>
-        <h1>React Area and Line Chart</h1>
+      <div className='mt-12'>
+        
         <CanvasJSChart options={options} />
       </div>
     );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Graph from '../components/Graph'
+import Graph from '../components/Graph';
 import Navbar from "../components/navbar";
+import Typist from 'react-typist';
 
 function Dashboard() {
     const [topGainers, setTopGainers] = useState([]);
     const [topLosers, setTopLosers] = useState([]);
+    const [selectedCSV, setSelectedCSV] = useState("");
 
     useEffect(() => {
         fetchTopGainersAndLosers();
@@ -12,7 +14,7 @@ function Dashboard() {
 
     const fetchTopGainersAndLosers = async () => {
         try {
-            const response = await fetch("http://localhost:3000/user/dashboard");
+            const response = await fetch(`http://localhost:3000/user/dashboard?csv=${selectedCSV}`);
             if (!response.ok) {
                 throw new Error("Failed to fetch data");
             }
@@ -24,26 +26,74 @@ function Dashboard() {
         }
     };
 
+    const handleCSVSelect = (csv) => {
+        setSelectedCSV(csv);
+    };
+
     return (
         <div>
             <Navbar />
             <div className="flex gap-10 p-5">
                 <div style={{ flex: '60%' }}>
-                    <Graph />
+                    <div>
+                        <h2 className="text-5xl">Select Your Choice Of Stock </h2>
+                        <Typist>
+                            <p className="text-lg">We are here for you! Here are your choice of stocks:</p>
+                        </Typist>
+                        <div>
+                            <button onClick={() => handleCSVSelect("AAPL")} className="p-2 rounded-md mr-2  border-solid border-2 border-black" >Apple</button>
+                            <button onClick={() => handleCSVSelect("ADANIGREEN")} className="p-2 rounded-md mr-2 border-solid border-2 border-black">Adani Green</button>
+                            <button onClick={() => handleCSVSelect("ADANIPOWER")} className="p-2 rounded-md mr-2 border-solid border-2 border-black">Adani Power</button>
+                            <button onClick={() => handleCSVSelect("APOLLOHOSP")} className="p-2 rounded-md mr-2 border-solid border-2 border-black">Apollo Hospitals</button>
+                            <button onClick={() => handleCSVSelect("HDFCBANK")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">HDFC bank</button>
+                            <button onClick={() => handleCSVSelect("HINDUNILVR")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">Hindustan Uniliver</button>
+                            <button onClick={() => handleCSVSelect("INFOSYS")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">Infosys</button>
+                            <button onClick={() => handleCSVSelect("IRCTC")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">IRCTC</button>
+                            <button onClick={() => handleCSVSelect("MSFT")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">Microsoft</button>
+                            <button onClick={() => handleCSVSelect("NFLX")} className="p-2 rounded-md mr-2 border-solid border-2 border-black  mt-5">Netflix</button>
+                            <button onClick={() => handleCSVSelect("ONGC")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">ONGC</button>
+                            <button onClick={() => handleCSVSelect("RELIANCE")} className="p-2 rounded-md mr-2 border-solid border-2 border-black ml-5 mt-5">RELIANCE</button>
+                            <button onClick={() => handleCSVSelect("SBIN")} className="p-2 rounded-md  border-solid border-2 border-black ml-5 mt-5">SBI</button>
+                            <button onClick={() => handleCSVSelect("TCS")} className="p-2 rounded-md  border-solid border-2 border-black ml-5 mt-5">TCS</button>
+                            <button onClick={() => handleCSVSelect("TECHM")} className="p-2 rounded-md  border-solid border-2 border-black ml-5 mt-5">Tech Mahindra</button>
+                            <button onClick={() => handleCSVSelect("ZYDUSLIFE")} className="p-2 rounded-md  border-solid border-2 border-black ml-5 mt-5">Zydus Life</button>
+                        </div>
+                    </div>
+                    <Graph selectedTicker={selectedCSV} />
                 </div>
                 <div style={{ flex: '1' }}>
                     <h2>Top Gainers</h2>
-                    <ul>
-                        {topGainers.map((gainer, index) => (
-                            <li key={index}>{gainer.ticker}: [{gainer.price}, {gainer.change_percentage}] </li>
+                    <div className="flex gap-4">
+                        {topGainers.slice(0, 2).map((gainer, index) => (
+                            <div key={index} className="bg-green-200 p-4 rounded-md w-32">
+                                <p>{gainer.ticker}</p>
+                                <p>Price: {gainer.price}</p>
+                                <p>Change Percentage: {gainer.change_percentage}</p>
+                            </div>
                         ))}
-                    </ul>
+                    </div>
                     <h2>Top Losers</h2>
-                    <ul>
-                        {topLosers.map((loser, index) => (
-                            <li key={index}>{loser.ticker}: [{loser.price},{loser.change_percentage}]</li>
+                    <div className="flex gap-4">
+                        {topLosers.slice(0, 2).map((loser, index) => (
+                            <div key={index} className="bg-red-200 p-4 rounded-md w-32">
+                                <p>{loser.ticker}</p>
+                                <p>Price: {loser.price}</p>
+                                <p>Change Percentage: {loser.change_percentage}</p>
+                            </div>
                         ))}
-                    </ul>
+                    </div>
+                    <h2 className="mt-4">Want news upadtes?</h2>
+                    <div className="flex gap-4">
+                        <div>
+                            <p>dsdmnjhbfgyueijwkfhrhuiwjkdnfh ruiwjkdnfriuwjkdnfheufjkjeknfkwjesn dsdmnjhbfgyueijwkfhrhuiwjkdnfh
+                            dsdmnjhbfgyueijwkfhrhuiwjkdnfh
+                            dsdmnjhbfgyueijwkfhrhuiwjkdnfh
+                            </p>
+
+                            <button className="bg-blue-200 p-4 rounded-2xl"> Check all upadtes</button>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
