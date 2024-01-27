30e8fc9ba5da849fc65ff7b1ff3f8bab8b340202
khushibansal13 <khushibansal1302@gmail.com>
2024-01-27 16:29:54 +0530

    landing change

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <header className="bg-white px-16 py-8 max-md:px-5">
       <nav className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[74%] max-md:w-full max-md:ml-0">
+        <div className="flex flex-col items-stretch w-[74%] max-md:w-full max-md:ml-0 -mr-96">
           <div className="flex items-center justify-center gap-8 text-3xl text-black font-medium tracking-tighter my-auto max-md:max-w-full max-md:flex-wrap max-md:mt-10">
             <a href="#" className="nav-link">
               Home
@@ -20,9 +20,9 @@ function Navbar() {
             </a>
           </div>
         </div>
-        <div className="flex flex-col items-stretch w-[26%] ml-5 max-md:w-full max-md:ml-0">
+        <div className="flex flex-col items-stretch w-max ml-5 max-md:w-full max-md:ml-0 ">
           <button
-            className="text-white text-2xl font-bold whitespace-nowrap bg-black grow justify-center items-stretch w-max px-7 py-5 rounded-[30px] max-md:mt-10 max-md:px-5 ml-auto"
+            className="text-white text-2xl font-bold whitespace-nowrap bg-black grow justify-center items-stretch w-max px-7 py-5 rounded-[30px] max-md:mt-10 max-md:px-5 ml-96"
             aria-label="Get Started Button"
           >
             Get Started
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -3,10 +3,27 @@ import Navbar from "../components/navbar"
 function Landing() {
     return(
         <>
-        <Navbar/>
-        
+    <Navbar/>
+      <div className="bg-slate-50 bg-opacity-20 flex w-full flex-col justify-center items-center px-16 py-12 max-md:max-w-full max-md:px-5">
+        <div className="flex w-full max-w-[941px] flex-col items-center mt-5 mb-9 max-md:max-w-full">
+          <img
+            loading="lazy"
+            srcSet="src\assets\landing_img.jpeg" className="aspect-[2.08] object-contain object-center w-[394px] max-w-full"
+            alt="Your Image"
+          />
+          <h1 className="text-black text-[180px] font-extrabold self-stretch mt-3.5 max-md:max-w-full max-md:text-4xl">
+            FORECOST
+          </h1>
+          <p className="text-black text-center text-2xl font-bold mt-7 max-md:max-w-full">
+            lorem impsum lorem impsum lorem impsum
+          </p>
+          <button className="text-white text-2xl font-bold whitespace-nowrap bg-black justify-center items-stretch mt-9 px-7 py-3.5 rounded-[30px] max-md:px-5">
+            Predict your fav stock
+          </button>
+        </div>
+        </div>
         </>
-    )
+  );
 }
-
+        
 export default Landing;
